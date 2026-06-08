c8b1a71bdea786780a306f58beb86cfa7887a022
nigusadmas21 <nigusadmas@gmail.com>
2026-06-08 13:05:14 +0100

    first-amazone-clone-commit

---
+++ Amazon-Clone-Project/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+   <div>
+    <h1>Amazon Clone Project</h1>
+   </div>
+    </>
+  )
+}
+
+export default App
